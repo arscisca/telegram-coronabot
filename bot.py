c416667cf3e15144c6afa684eeea2983a48f2d88
Alessandro Scisca <s276032@studenti.polito.it>
2020-10-10 11:27:09 +0000

    Simplify API token reading

--- bot.py
+++ bot.py
@@ -1,27 +1,15 @@
+import sys
 import logging
 from telegram.ext import Updater
 
 import handlers
 
 
-def read_token(file):
-    """Read Telegram API token from specified file
-    Args:
-        file (file)
-    Return:
-        str: API token
-    """
-    return file.readline().strip()
-
-
-def main():
+def main(token):
     """Main function"""
     # Setup logging
     logging.basicConfig(filename='logs/backend.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
-    # API token
-    with open('.token', 'r') as f:
-        token = read_token(f)
     # Initialize bot
     updater = Updater(token=token, use_context=True)
     dispatcher = updater.dispatcher
@@ -35,4 +23,10 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    token = ''
+    try:
+        token = sys.argv[1]
+    except IndexError:
+        print("Please pass Telegram API token as a program argument.")
+        exit(-1)
+    main(token)
